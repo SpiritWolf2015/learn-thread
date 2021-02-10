56c9d564a70b3812a2312035bdd5215aaf643563
SpiritWolf <522414019@qq.com>
2021-02-10 22:04:26 +0800

    [GZC]第5章原始示例程序

--- ThreadDesignPatterns/src/com/gzc/chapter5/ori_sample/Table.java
+++ ThreadDesignPatterns/src/com/gzc/chapter5/ori_sample/Table.java
@@ -1,11 +1,10 @@
-package com.gzc.chapter5.sample;
+package com.gzc.chapter5.ori_sample;
 
 public class Table {
     private final String[] buffer;
     private int tail;  // 下次put的位置
     private int head;  // 下次take的位置
     private int curCount; // buffer中的蛋糕个数
-    private int id;
 
     public Table(int maxCount) {
         this.buffer = new String[maxCount];
@@ -14,33 +13,19 @@ public class Table {
         this.curCount = 0;
     }
 
-    private int nextId() {
-        id++;
-        return id;
-    }
-
-    /**
-     * 放置蛋糕
-     * @param threadName
-     * @throws InterruptedException
-     */
-    public synchronized void put(String threadName) throws InterruptedException {
+    // 放置蛋糕
+    public synchronized void put(String cake) throws InterruptedException {
+        System.out.println("-----------------" + Thread.currentThread().getName() + " puts " + cake);
         while (curCount >= buffer.length) {
             wait();
         }
-        String cake = "[ Cake No." + nextId() + " by " + threadName + " ]"; //注意，放蛋糕要放在while循环后面
-        System.out.println(Thread.currentThread().getName() + " puts " + cake);
         buffer[tail] = cake;
         tail = (tail + 1) % buffer.length;
         curCount++;
         notifyAll();
     }
 
-    /**
-     * 取蛋糕
-     * @return
-     * @throws InterruptedException
-     */
+    // 取蛋糕
     public synchronized String take() throws InterruptedException {
         while (curCount <= 0) {
             wait();
@@ -49,7 +34,7 @@ public class Table {
         head = (head + 1) % buffer.length;
         curCount--;
         notifyAll();
-        System.out.println("-------------" + Thread.currentThread().getName() + " takes " + cake);
+        System.out.println(Thread.currentThread().getName() + " takes " + cake);
         return cake;
     }
 }
