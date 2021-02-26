d02e32c81e4e8c9dfe0ebf656f82c1547c6510a8
SpiritWolf <522414019@qq.com>
2021-02-27 00:42:32 +0800

    [GZC]第6章，使用并发包API的示例程序

---
+++ ThreadDesignPatterns/src/com/gzc/chapter6/jucSample/ReaderThread.java
@@ -0,0 +1,19 @@
+package com.gzc.chapter6.jucSample;
+
+public class ReaderThread extends Thread {
+    private final Data data;
+    public ReaderThread(Data data) {
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                char[] readbuf = data.read();
+                System.out.println(Thread.currentThread().getName() + " reads " + String.valueOf(readbuf));
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+}
