e30a23311352106accf19157de098a6b1c4b74c7
SpiritWolf <522414019@qq.com>
2021-02-28 16:13:18 +0800

    [GZC]第8章示例程序

---
+++ ThreadDesignPatterns/src/com/gzc/chapter8/sample/WorkerThread.java
@@ -0,0 +1,18 @@
+package com.gzc.chapter8.sample;
+
+public class WorkerThread extends Thread {
+    private final Channel channel;
+
+    public WorkerThread(String name, Channel channel) {
+        super(name);
+        this.channel = channel;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            Request request = channel.takeRequest();
+            request.execute();
+        }
+    }
+}
