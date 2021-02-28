a483edd098d56730a079e8ca687e49d93684c8c2
SpiritWolf <522414019@qq.com>
2021-02-28 18:04:18 +0800

    [GZC]第8章，通过ExecutorService实现示例程序

---
+++ ThreadDesignPatterns/src/com/gzc/chapter8/jucSample/Request.java
@@ -0,0 +1,28 @@
+package com.gzc.chapter8.jucSample;
+
+import java.util.Random;
+
+public class Request implements Runnable {
+    private final String name; // 委托者
+    private final int number;  // 请求的编号
+    private static final Random random = new Random();
+
+    public Request(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " executes " + this);
+        try {
+            Thread.sleep(random.nextInt(1000));
+        } catch (InterruptedException e) {
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[ Request from " + name + " No." + number + " ]";
+    }
+}
